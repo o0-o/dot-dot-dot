3dbc7f7764d9f468fad135672b4f348a301d4c0c
oØ.o <o0-o@dotdigital.digital>
2020-09-03 14:07:42 -0400

    Remove iterm2, terminal is fine, no need for it

--- .config/zsh/zshrc.d/90-iterm.zsh
+++
@@ -1,9 +0,0 @@
-### ITERM ZSH
-
-test -e "${HOME}/.iterm2_shell_integration.zsh"   &&
-source "${HOME}/.iterm2_shell_integration.zsh"    ||
-! test -e "${HOME}/.iterm2_shell_integration.zsh" ||
-
-return 1
-
-return 0
