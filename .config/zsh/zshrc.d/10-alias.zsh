aa8bf5d80d0e79ff3af3edd490788eeb9b045fba
oØ.o <o0-o@dotdigital.digital>
2020-10-28 20:47:14 -0400

    add nvim aliases and update formatting

--- .config/zsh/zshrc.d/10-alias.zsh
+++ .config/zsh/zshrc.d/10-alias.zsh
@@ -1,27 +1,29 @@
 ### ALIASES ZSH
 
-declare -a ALIASES=(
+declare -a aliases=(
   'cat'   "ccat --bg='dark' --color='auto'" '/dev/null'
-)     &&
+  'vi'    'nvim'                            '--version'
+  'vim'   'nvim'                            '--version'
+)                                             &&
 
-for COMMAND ALIAS TEST in "${ALIASES[@]}"; do
+for command alias test in "${aliases[@]}";    do
   # the command used in alias without options/arguments
-  ALIAS_COMMAND="$( printf '%s' "${COMMAND}" |
-                      cut -d ' ' -f '1'
-                  )"                        &&
+  alias_command="$( printf '%s' "${command}"  |
+                    cut -d ' ' -f '1'
+                  )"                          &&
 
   # does alias command exist
-  command -v "${ALIAS_COMMAND}" &>/dev/null &&
-  alias "${COMMAND}"="${ALIAS}"             &&
+  command -v "${alias_command}"   &>/dev/null &&
+  alias "${command}"="${alias}"               &&
 
   # test the alias and unset on failure
-  { "${COMMAND}" "${TEST}" &>/dev/null  ||
-    unalias "${COMMAND}"
-  }                                         ||
+  { "${command}" "${test}"        &>/dev/null ||
+    unalias "${command}"
+  }                                           ||
 
   # let alias fail if $ALIAS command isn't available
-  ! command -v "${ALIAS_COMMAND}" &>/dev/null
-done  ||
+  ! command -v "${alias_command}" &>/dev/null
+done                                          ||
 
 return 1
 
