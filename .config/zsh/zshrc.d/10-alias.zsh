d811d2b6edabaec9573787b49821a8ad84147d6b
oØ.o <o0-o@dotdigital.digital>
2020-12-04 01:32:23 -0500

    simplify zsh alias assignments

--- .config/zsh/zshrc.d/10-alias.zsh
+++ .config/zsh/zshrc.d/10-alias.zsh
@@ -6,28 +6,15 @@ declare -a alias_assignments=(
   'vi'    'nvim'                            '--version'
   'vim'   'nvim'                            '--version'
 
-)                                                     &&
+)                                                       &&
 
 for the_alias alias_def alias_test in "${alias_assignments[@]}"; do
-  # clear pre-existing alias
-  unalias "${the_alias}"                  2>/dev/null || :
 
-  # the command used in alias without options/arguments
-  alias_def_cmd="$( printf '%s' "${alias_def}"  |
-                    cut -d ' ' -f '1'             )"  &&
+  # does alias command pass test
+  "${alias_def}" ${alias_test}        &>/dev/null &&
+  alias "${the_alias}"="${alias_def}"             || :
 
-  # does alias command exist
-  command -v "${alias_def_cmd}"           &>/dev/null &&
-  alias "${the_alias}"="${alias_def}"                 &&
-
-  # test the alias and unset on failure
-  { "${the_alias}" "${alias_test}"        &>/dev/null ||
-    unalias "${the_alias}"
-  }                                                   ||
-
-  # let alias fail if $alias command isn't available
-  ! command -v "${alias_def_cmd}"         &>/dev/null
-done                                                  ||
+done                                              ||
 
 return 1
 
