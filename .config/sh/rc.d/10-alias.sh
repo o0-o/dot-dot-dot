a2a178829642392dd366faf77b1484eb627cdec1
oØ.o <o0-o@dotdigital.digital>
2021-02-21 01:54:47 -0500

    fix vi to vim/nvim soft link

--- .config/sh/rc.d/10-alias.sh
+++ .config/sh/rc.d/10-alias.sh
@@ -3,9 +3,11 @@ alias 'ssh'='TERM=screen-256color ssh'                      ||
 [ ! "${TERM}" = alacritty ]
 
 ln  -sf "$(command which nvim)"                 \
-        "${XDG_BIN_HOME-$HOME/.local/bin}/vim"  2>/dev/null ||
-! command -v nvim                               >/dev/null
-
-ln  -sf "$(command which vim)"                  \
-        "${XDG_BIN_HOME-$HOME/.local/bin}/vi"   2>/dev/null ||
-! command -v vim                                >/dev/null
+        "${XDG_BIN_HOME-$HOME/.local/bin}/vim"  2>/dev/null &&
+ln  -sf "$(command which nvim)"                 \
+        "${XDG_BIN_HOME-$HOME/.local/bin}/vi"   ||
+{ ! command -v nvim                             >/dev/null  &&
+  ln  -sf "$(command which vim)"                \
+          "${XDG_BIN_HOME-$HOME/.local/bin}/vi" 2>/dev/null ||
+  ! command -v vim                              >/dev/null
+}
